38867bc18cb329b748e1d05912503ad6eb95a0c8
Nanci-0227 <286843590+Nanci-0227@users.noreply.github.com>
2026-09-08 15:39:44 +0800

    Wait for local web server before opening browser

--- start_web.bat
+++ start_web.bat
@@ -1,4 +1,4 @@
 @echo off
+start "" /b "%~dp0.venv\Scripts\python.exe" "%~dp0project\web_app.py"
+timeout /t 2 /nobreak >nul
 start "" http://127.0.0.1:5000
-"%~dp0.venv\Scripts\python.exe" "%~dp0project\web_app.py"
-if errorlevel 1 pause
